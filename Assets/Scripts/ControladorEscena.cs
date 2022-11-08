f2e73ef53aac58d052d85582cdd63790ab1b1186
AinhoaLonP <ainhoalonpe@gmail.com>
2022-11-08 16:40:57 +0100

    First commit

---
+++ Assets/Scripts/ControladorEscena.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControladorEscena : MonoBehaviour
+{
+    public GameObject canvasPerder;
+    public AudioSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void Perder()
+    {
+        canvasPerder.SetActive(true);
+        Time.timeScale = 0;
+        audio.Stop();
+    }
+
+    public void Reiniciar()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/Scripts/LogicaObstaculo.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class LogicaObstaculo : MonoBehaviour
+{
+    public float velocidad;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.left * velocidad * Time.deltaTime;
+    }
+}
